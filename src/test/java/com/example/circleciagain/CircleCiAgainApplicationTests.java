13f7159b5520ec2dececaf0f19ba94e40f331fa0
Devin Leyba <dleyba042@gmail.com>
2022-11-29 13:39:06 -0800

    created

---
+++ src/test/java/com/example/circleciagain/CircleCiAgainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.circleciagain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleCiAgainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
